12526b6a4c61902869edb44804c08dc50c317680
MohitSoni522 <153983719+MohitSoni522@users.noreply.github.com>
2023-12-18 17:42:46 +0530

    Update test.js

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
 console.log("Mohit")
+
+const account_id = 1;
+let account_name = "BOB";
+var account_city = "Delhi";
+account_state = "Delhi";
+
+connsole.log(account_id);
+
+connsole.table(account_id, account_name, account_city, account_state);
